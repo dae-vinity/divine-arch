f649878d71c1c3df95e050c25adeccf8e11d4d0a
daemon <dae_vinity@proton.me>
2026-02-19 10:20:03 -0600

    xml to map should be implemented i hope

--- divine.go
+++ divine.go
@@ -3,55 +3,77 @@ package main
 import (
 	"encoding/xml"
 	"fmt"
-	"log"
+	"io"
 	"os"
-	"strings"
 
-	// "github.com/76creates/stickers/flexbox"
 	tea "github.com/charmbracelet/bubbletea"
 )
 
-type model struct {
-	screen []string
+type dict map[string]any
+
+type element struct {
+	XMLName  xml.Name
+	NameAttr xml.Attr   `xml:"name,attr"`
+	Attrs    []xml.Attr `xml:",any,attr"`
+	Elements []element  `xml:",any"`
+	Content  string     `xml:",chardata"`
 }
 
-func initialModel() model {
-	modelFile, err := os.ReadFile("model.xml")
-	if err != nil {
-		log.Fatal(err)
+func convertXMLToInst(xmlElement element) dict {
+	var instance dict = make(dict)
+	instance["class"] = xmlElement.XMLName.Local
+	if xmlElement.NameAttr.Value != "" {
+		instance["name"] = xmlElement.NameAttr.Value
+	} else {
+		instance["name"] = instance["class"]
 	}
-	var model model
-	xml.Unmarshal(modelFile, &model)
+	instance["content"] = xmlElement.Content
+	for i := 0; i < len(xmlElement.Elements); i++ {
+		var child dict = convertXMLToInst(xmlElement.Elements[i])
+		instance[child["name"].(string)] = child
+	}
+	for i := 0; i < len(xmlElement.Attrs); i++ {
+		instance[xmlElement.Attrs[i].Name.Local] = xmlElement.Attrs[i].Value
+	}
+	return instance
+}
+
+func initialModel() dict {
+	xmlFile, err := os.Open("model.xml")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer xmlFile.Close()
+	byteValue, _ := io.ReadAll(xmlFile)
+	var xmlModel element
+	xml.Unmarshal(byteValue, &xmlModel)
+
+	var model dict = convertXMLToInst(xmlModel)
 	return model
 }
 
-func (model model) Init() tea.Cmd {
+func (model dict) Init() tea.Cmd {
 	return nil
 }
 
-func (model model) Update(message tea.Msg) (tea.Model, tea.Cmd) {
+func (model dict) Update(message tea.Msg) (tea.Model, tea.Cmd) {
 	switch message := message.(type) {
 
-		case tea.KeyMsg:
-        switch message.String() {
+	case tea.KeyMsg:
+		switch message.String() {
 
-        case "ctrl+c", "q":
-            return model, tea.Quit
+		case "ctrl+c", "q":
+			return model, tea.Quit
 		}
 	}
 	return model, nil
 }
 
-func (model model) View() string {
-	var screen strings.Builder
-
-	screen.WriteString("")
-
-	return screen.String()
+func (model dict) View() string {
+	return model["name"].(string)
 }
 
 func main() {
-	
 	program := tea.NewProgram(initialModel(), tea.WithAltScreen())
 	if _, err := program.Run(); err != nil {
 		fmt.Printf("you... IDIOT. %v", err)
